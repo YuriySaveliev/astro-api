e5612b2cd4a1ef53c1c5332cd0909846f8012432
Yuriy <savjurnick@gmail.com>
2024-05-14 10:39:51 +0300

    Light refactoring

--- astro-api.py
+++ astro-api.py
@@ -2,13 +2,15 @@ import requests
 import csv
 import json
 
-url = 'http://api.open-notify.org/astros.json'
-people_file_name = 'people.csv'
-headers = ['name', 'craft']
+URL = 'http://api.open-notify.org/astros.json'
+PEOPLE_FILE_NAME = 'people.csv'
+HEADERS = ['name', 'craft']
+
 
 def get_people_in_space_list(request_url):
     r = requests.get(request_url)
     return json.loads(r.content)['people']
+
 
 def write_people_to_file(file_name, file_headers, people_list):
     with open(file_name, 'w') as csv_file:
@@ -16,13 +18,6 @@ def write_people_to_file(file_name, file_headers, people_list):
         for row in people_list:
             writer.writerow(row)
 
-def get_craft_list(people_list):
-    crafts = []
-
-    for row in people_list:
-        crafts.append(row['craft'])
-
-    return crafts
 
 def write_crafts_people(file_headers, people_list, crafts_list):
     for craft in crafts_list:
@@ -32,12 +27,12 @@ def write_crafts_people(file_headers, people_list, crafts_list):
                 if row['craft'] == craft:
                     writer.writerow(row)
 
+
 def init():
-    crafts = []
-    people_in_space = get_people_in_space_list(url)
-    write_people_to_file(people_file_name, headers, people_in_space)
-    crafts = get_craft_list(people_in_space)
-    crafts = set(crafts)
-    write_crafts_people(headers, people_in_space, crafts)
+    people_in_space = get_people_in_space_list(URL)
+    crafts = set(row['craft'] for row in people_in_space)
+    write_people_to_file(PEOPLE_FILE_NAME, HEADERS, people_in_space)
+    write_crafts_people(HEADERS, people_in_space, crafts)
+
 
 init()
