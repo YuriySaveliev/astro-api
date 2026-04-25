3bbbb9482673e5d17876005ab05bc62b76d3b660
Yuriy Savelyev <savjurnick@gmail.com>
2026-04-25 17:41:15 +0300

    Add logging, fix types

--- astro-api.py
+++ astro-api.py
@@ -1,16 +1,21 @@
 import requests
 import csv
 import json
+import logging
 
 URL = 'http://api.open-notify.org/astros.json'
 PEOPLE_FILE_NAME = 'people.csv'
 HEADERS = ['name', 'craft']
 
 
-def get_people_in_space(request_url: str) -> json:
-    print('Calling API...')
-    r = requests.get(request_url).content
-    print('Data fetched')
+def get_people_in_space(request_url: str) -> any:
+    logging.info('Calling API...')
+    try:
+        r = requests.get(request_url, timeout=5).content
+    except Exception as err:
+        logging.error(err)
+        return None
+    logging.info('Data fetched')
     return json.loads(r)['people']
 
 
@@ -23,24 +28,32 @@ def write_to_csv(file_name: str, file_headers: list[str], items: list[str]) -> N
 
 
 def write_people_to_file(file_name: str, file_headers: list[str], people_list: list[str]) -> None:
-    print('Writing people to CSV')
+    logging.info('Writing people to CSV')
     write_to_csv(file_name, file_headers, people_list)
 
 
-def write_crafts_people(file_headers: list[str], people_list: list[str], crafts_list: list[str]) -> None:
-    print('Writing crafts to CSV')
+def sanitize_filename(filename: str) -> str:
+    return filename.replace(' _:', '')
+                            
+
+def write_crafts_people(file_headers: list[str], people_list: list[str], crafts_list: set[str]) -> None:
+    logging.info('Writing crafts to CSV')
     for craft in crafts_list:
-        file_name = f'{craft}.csv'
+        file_name = f'{sanitize_filename(craft)}.csv'
         people_list_filtered = filter(lambda man: man['craft'] == craft, people_list)
         write_to_csv(file_name, file_headers, people_list_filtered)
 
 
-def start() -> None:
-    print('Starting script')
+def main() -> None:
+    logging.basicConfig(level=logging.INFO)
+    logging.info('Starting script')
+
     people = get_people_in_space(URL)
     crafts = set(row['craft'] for row in people)
+
     write_people_to_file(PEOPLE_FILE_NAME, HEADERS, people)
     write_crafts_people(HEADERS, people, crafts)
 
 
-start()
+if __name__ == '__main__':
+    main()
